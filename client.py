695eabf2a6e39bfb165b6380cf909692f60916fe
Andrei Mihai <andreimihai2305is@gmail.com>
2024-09-02 12:02:33 +0300

    establishing proper connection between client and server

--- client.py
+++ client.py
@@ -1,13 +1,14 @@
 import pygame
 import sys
+import socket
+import json
 from common import *
-from common import PlayerCommon
 
 pygame.init()
 
 class Player(PlayerCommon):
-    def __init__(self, x, y) -> None:
-        super().__init__(x, y)
+    def __init__(self, x, y, conn) -> None:
+        super().__init__(x, y, conn)
         self.rect = pygame.Rect(self.x, self.y, 60, 60)
 
     def draw_self(self, screen : pygame.Surface):
@@ -15,14 +16,29 @@ class Player(PlayerCommon):
 
 
 def main() -> None:
-    
     screen : pygame.Surface = pygame.display.set_mode((WIDTH, HEIGHT))
     clock : pygame.time.Clock = pygame.time.Clock()
     pygame.display.set_caption("Multiplayer Demo")
-    p = Player(10, 10)
+    
+    conn_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    conn_socket.connect((HOST, PORT))
+    p = Player(10, 10, conn_socket)
+
+    try:
+        mess = json.loads(p.conn.recv(1024))
+        if mess["type"] == "hello":
+            p.conn.sendall(json.dumps(mess).encode("utf-8"))
+        print(mess)
+    
+    except Exception as e:
+        print(e)
+        p.conn.close()
+
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                p.conn.close()
                 pygame.quit()
                 print("Exit Succesfully")
                 sys.exit(0)
--- server.py
+++ server.py
@@ -4,12 +4,13 @@ import json
 import logging
 from common import *
 
+
 type Err = Exception | None
+
 
 class Player(PlayerCommon):
     def __init__(self, x, y, conn) -> None:
         super().__init__(x, y, conn)
-
 
 
 
@@ -24,21 +25,20 @@ def close_conn(conn, addr, with_err: Err = None) -> None:
 
 
 def handle_client(client : socket.socket, addr : str) -> None:
-    client.sendall(json.dumps({
-        "type" : "hello"
-    }))
-
     try:
+        client.sendall(json.dumps({"type" : "hello"}).encode("utf-8"))
+
         hello_msg = client.recv(1024)
         hello_msg : dict = json.loads(hello_msg)
 
         if hello_msg["type"] != "hello":
             close_conn(client, addr)
+        else:
+            print(hello_msg)
 
 
     except Exception as e:
         close_conn(client, addr, e)
-
 
     while True:
         try:
@@ -48,14 +48,20 @@ def handle_client(client : socket.socket, addr : str) -> None:
             close_conn(client, addr, e)
             break
         
+if __name__ == "__main__":
+    format = "%(asctime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
 
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((HOST, PORT))
-server.listen(5)
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((HOST, PORT))
+    
+    logging.info(f"Server is now listening on port: {PORT}")
+    server.listen(5)
 
 
-while True:
-    client_sock, addr = server.accept()
-    client_thread = threading.Thread(target=handle_client, args=(client_sock, addr))
-    client_thread.start()
+    while True:
+        client_sock, addr = server.accept()
+        client_thread = threading.Thread(target=handle_client, args=(client_sock, addr))
+        client_thread.start()
+
 
